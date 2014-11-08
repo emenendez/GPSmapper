870584711ee9aeb0da0072fa54016b9ef30de051
Eric Menendez <ericmenendez@gmail.com>
2014-11-08 15:08:58 -0500

    Use Esri topo basemap instead of OSM

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,10 +10,9 @@ var defaultCenter = L.latLng(39, -78)
 
 var map = L.map('map').setView(defaultCenter, 13);
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 }).addTo(map);
-
 
 // Init Dropbox
 client.authenticate({interactive: false}, function(error, client) {
