47992b9f1dd37d3f6f0288ce32a46a12aa73e9a5
Eric Menendez <ericmenendez@gmail.com>
2014-11-08 11:22:11 -0500

    Basic dropbox.js wireframing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,83 @@
-console.log('\'Allo \'Allo!');
+var client = new Dropbox.Client({ key: '9a666eiuctz1yh4' });
+
+var cursorTag = null;
+
+client.authenticate({interactive: false}, function(error, client) {
+  if (error) {
+    return showError(error);
+  }
+
+  if (client.isAuthenticated()) {
+  	run(client);
+  }
+  else {
+  	client.authenticate(function(error, client) {
+  		if (error) {
+  			return showError(error);
+  		}
+  		run(client);
+  	});
+  }
+
+});
+
+function pullChanges() {
+  client.pullChanges(function(error, changes) {
+    if (error) {
+      return showError(error);
+    }
+
+    cursorTag = changes.cursorTag;
+
+    processChanges(changes.changes);
+  });
+}
+
+
+function processChanges(changes) {
+  changes.forEach(function(change) {
+    if (change.wasRemoved) {
+      console.log('Removed: ' + change.path);
+    }
+    else {
+      console.log(change.path);
+    }
+  });
+
+  if (changes.shouldBackOff) {
+    interval = 5000;
+  }
+  else {
+    interval = 0;
+  }
+
+  setTimeout(poll, interval);
+}
+
+function poll() {
+  console.log('Polling for changes...');
+  client.pollForChanges(cursorTag, function(error, result) {
+    if (error) {
+      return showError(error);
+    }
+
+    if (result.hasChanges) {
+      console.log('Changes detected.');
+      pullChanges();
+    }
+    else {
+      console.log('No changes detected.');
+      setTimeout(poll, result.retryAfter * 1000);
+    }
+  });
+}
+
+function run(client) {
+	console.log('running');
+
+  pullChanges();
+}
+
+function showError(error) {
+	console.log(error);
+}
